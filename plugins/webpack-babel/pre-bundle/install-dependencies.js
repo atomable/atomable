4e6c4804cc0d0c05260bd674ed28fa1819dbee91
pre63 <sd@pre63.com>
2016-11-13 14:22:11 -0500

    updated dependencies

--- plugins/webpack-babel/pre-bundle/install-dependencies.js
+++ plugins/webpack-babel/pre-bundle/install-dependencies.js
@@ -5,7 +5,7 @@ module.exports = tmp =>
     exec('npm install --dev '
       + 'babel-preset-latest babel-preset-stage-0 babel-plugin-transform-runtime '
       + 'atomable-runtime source-map-loader eslint-loader babel-runtime babel-loader '
-      + 'babel-core babel-cli', {
+      + 'babel-core babel-cli ', {
         cwd: tmp,
       }, (error, stdout, stderr) => {
         if (error) {
